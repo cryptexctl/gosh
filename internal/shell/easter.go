575aa37f971a5bdbeafcd2102792e6bccd5a157d
Lain Iwakura <lain@iwakurahome.ru>
2025-06-28 14:40:13 +0300

    updated easter eggs

--- internal/shell/easter.go
+++ internal/shell/easter.go
@@ -3,16 +3,28 @@
 
 package shell
 
-import "gosh/internal/builtin"
+import (
+	"fmt"
+	"gosh/internal/builtin"
+	"os"
+)
 
 func registerEaster(b *builtin.Manager) {
 	b.Register("gosha", func(args []string) int {
-		println("Это не смешно!")
+		path, _ := os.Executable()
+		fmt.Printf("Это не смешно! (%s)\n", path)
 		return 0
 	})
 
 	b.Register("bash", func(args []string) int {
-		println("Bash is too old.")
+		path, _ := os.Executable()
+		fmt.Printf("Bash is too old. Try %s\n", path)
+		return 0
+	})
+
+	b.Register("oh my", func(args []string) int {
+		path, _ := os.Executable()
+		fmt.Printf("gosh! (%s)\n", path)
 		return 0
 	})
 }
